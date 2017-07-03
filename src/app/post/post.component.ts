3a40921a830869db6568fa287ecacaa63f3d1419
wozaizhelo <1845882620@qq.com>
2017-07-03 22:53:21 +0800

    page commit

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -8,17 +8,41 @@ import { PostListService } from "./service/postList.service";
   providers:[PostListService]
 })
 export class PostComponent implements OnInit {
-  public authorList;
-  public total;
+  public authorList;/*列表数组*/
+  public getList;/*对应的条数*/
+  public total;/*总条数*/
+  public page;/*总页数*/
+  public p = 1;/*初始化首页*/
+  public pageArr = [];
   constructor(private postListService:PostListService) { }
   getAuthor():void{
   	this.postListService.getAuthorList().then(data=>{
   		this.authorList = JSON.parse(data["_body"]).items;
   		this.total = JSON.parse(data["_body"]).total;
-  		console.log(this.authorList);
+      this.page = Math.ceil(this.total/5);
+      for(let i=0;i<this.page;i++){
+        this.pageArr.push(i+1);
+      }
+      if(this.total > 5){ 
+        this.getList = this.authorList.slice(0,5); 
+      }
   	}).catch(err=>{
   		console.log(err);
   	});
+  }
+  skipPage(page){
+    if(page == '+1'){
+      this.p++;
+    }else if(page == '-1'){
+      this.p--;
+    }else{
+      this.p = page;
+    }
+    if(page == this.page){
+      this.getList = this.authorList.slice(5*(this.p-1));
+    }else{
+      this.getList = this.authorList.slice(5*(this.p-1),5*(this.p-1)+5);
+    }
   }
   ngOnInit() {
   	this.getAuthor();
