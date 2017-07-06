d5078927e4dcbfee08c3bfa9f363b0f9fc0ae2a0
curry <17828455424@163.com>
2017-07-06 13:36:13 +0800

    微信支付提交

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { PostListService } from "./service/postList.service";
 
+/*import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/fromEvent';*/
+import { Observable } from "rxjs";
+
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
   providers:[PostListService]
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit { 
   public authorList;/*列表数组*/
   public getList;/*对应的条数*/
   public total;/*总条数*/
   public page;/*总页数*/
   public p = 1;/*初始化首页*/
   public pageArr = [];
+
+  /*用于搜索查找的方式*/
+  public obser = Observable.fromEvent(document.getElementById("search"),"keyup");
   constructor(private postListService:PostListService) { }
   getAuthor():void{
   	this.postListService.getAuthorList().then(data=>{
